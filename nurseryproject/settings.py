3d390c32b96153f71811cbdf36737b1ad25ce4bb
Sudhir Kumar <sudhir1041@gmail.com>
2025-05-02 12:49:17 +0530

    Add: whatsapp in status badge update template

--- nurseryproject/settings.py
+++ nurseryproject/settings.py
@@ -18,8 +18,8 @@ from dotenv import load_dotenv
 from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-from channels.layers import get_channel_layer
-channel_layer = get_channel_layer()
+# from channels.layers import get_channel_layer
+# channel_layer = get_channel_layer()
 
 
 # --- Load Environment Variables ---
