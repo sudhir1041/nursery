d616165a3baec67c44f50429574665f876534f91
Sudhir Kumar <sudhir1041@gmail.com>
2025-05-19 13:25:09 +0530

    card css optimize add for  shipping card

--- shipment_app/urls.py
+++ shipment_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views 
+from . import views
 
-app_name = 'shipment' 
+app_name = 'shipment'
 
 urlpatterns = [
-    path('', views.home, name='shipment_index'), 
+    path('',views.home,name='shipment_index'),
     path('process-shipment/', views.process_shipment, name='process_shipment'),
-    # path('clone-order/<str:order_id>/', views.clone_order, name='clone_order_detail'), # Example for your existing clone
+    # path('clone-order/<str:order_id>/', views.clone_order, name='clone_order_detail'),
 ]
