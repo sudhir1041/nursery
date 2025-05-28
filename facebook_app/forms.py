8a02d289ba4ad621ccc1fe7c6a907a4e0ee3e35e
Sudhir Kumar <sudhir1041@gmail.com>
2025-05-28 12:10:31 +0530

    facebook app in update form

--- facebook_app/forms.py
+++ facebook_app/forms.py
@@ -4,6 +4,49 @@ import json
 
 class FacebookOrderForm(forms.ModelForm):
 
+    INDIAN_STATES = [
+        ('', 'Select State'),
+        ('AN', 'Andaman and Nicobar Islands'),
+        ('AP', 'Andhra Pradesh'),
+        ('AR', 'Arunachal Pradesh'),
+        ('AS', 'Assam'),
+        ('BR', 'Bihar'),
+        ('CH', 'Chandigarh'),
+        ('CT', 'Chhattisgarh'),
+        ('DN', 'Dadra and Nagar Haveli'),
+        ('DD', 'Daman and Diu'),
+        ('DL', 'Delhi'),
+        ('GA', 'Goa'),
+        ('GJ', 'Gujarat'),
+        ('HR', 'Haryana'),
+        ('HP', 'Himachal Pradesh'),
+        ('JK', 'Jammu and Kashmir'),
+        ('JH', 'Jharkhand'),
+        ('KA', 'Karnataka'),
+        ('KL', 'Kerala'),
+        ('LA', 'Ladakh'),
+        ('LD', 'Lakshadweep'),
+        ('MP', 'Madhya Pradesh'),
+        ('MH', 'Maharashtra'),
+        ('MN', 'Manipur'),
+        ('ML', 'Meghalaya'),
+        ('MZ', 'Mizoram'),
+        ('NL', 'Nagaland'),
+        ('OR', 'Odisha'),
+        ('PY', 'Puducherry'),
+        ('PB', 'Punjab'),
+        ('RJ', 'Rajasthan'),
+        ('SK', 'Sikkim'),
+        ('TN', 'Tamil Nadu'),
+        ('TG', 'Telangana'),
+        ('TR', 'Tripura'),
+        ('UP', 'Uttar Pradesh'),
+        ('UT', 'Uttarakhand'),
+        ('WB', 'West Bengal')
+    ]
+
+    billing_state = forms.ChoiceField(choices=INDIAN_STATES, widget=forms.Select(attrs={'class': 'form-control'}))
+
     def clean_products_json(self):
 
         data = self.cleaned_data.get('products_json')
