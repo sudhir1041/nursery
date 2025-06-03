166ce349f1e261a4e723043d95804f6ea339cc3f
Sudhir Kumar <sudhir1041@gmail.com>
2025-06-03 11:42:57 +0530

    updates

--- shipment_app/urls.py
+++ shipment_app/urls.py
@@ -5,6 +5,7 @@ app_name = 'shipment'
 
 urlpatterns = [
     path('',views.home,name='shipment_index'),
+    path('shipped',views.shipped, name="already_shipped"),
     path('process-shipment/', views.process_shipment, name='process_shipment'),
     # path('clone-order/<str:order_id>/', views.clone_order, name='clone_order_detail'),
 ]
