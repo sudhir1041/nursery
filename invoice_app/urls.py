01d07b04d3f5d342345456c7df2aa3e6fc0c1909
Sudhir Kumar <sudhir1041@gmail.com>
2025-06-13 11:19:40 +0530

    Invoiceing ssyem

--- invoice_app/urls.py
+++ invoice_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'invoice_app'
+
 urlpatterns = [
     path('invoice/<int:invoice_id>/download/', views.download_invoice_pdf, name='download_invoice_pdf'),
 ]
